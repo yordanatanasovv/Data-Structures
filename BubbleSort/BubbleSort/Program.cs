e9e9a3e6c428c4e9253df19e939b01546ba41b68
yordanatanasovv <atanasovyordan321@gmail.com>
2019-05-15 00:13:33 +0300

    Bubble Sort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 31, 52, 13, 24, 75, 76, 87, 88, 49 };
+
+            Console.WriteLine($"Numbers: {string.Join(", ", numbers)}");
+
+
+            BubbleSort(numbers);
+        }
+
+        public static void BubbleSort(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sorted numbers: {string.Join(", ", numbers)}");
+        }
+    }
+}
