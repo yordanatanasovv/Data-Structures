450eeb9d48eb81f812e7832b8d044c9f335e5712
yordanatanasovv <atanasovyordan321@gmail.com>
2019-05-15 13:12:20 +0300

    Insertion Sort

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 0, 6, 3, 2, 8, 1 };
+
+            List<int> result = InsertionSort(numbers);
+
+            Console.WriteLine($"Sorted numbers: {string.Join(", ", numbers)}");
+        }
+
+        public static List<int> InsertionSort(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int current = numbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j] > current)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j -= 1;
+                }
+
+                numbers[j + 1] = current;
+            }
+
+            return numbers;
+        }
+    }
+}
